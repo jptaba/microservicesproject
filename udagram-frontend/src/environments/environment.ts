6220f33c7bdf558ba93a52ccc55cd4b54ef8802d
jptaba <92991909+jptaba@users.noreply.github.com>
2021-12-03 22:41:26 -0500

    changes

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a477576468fd9455ca1ee97033bbb491-1999462242.us-east-2.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
